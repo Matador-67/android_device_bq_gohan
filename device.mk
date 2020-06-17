f143a76d87d6ee16113d8cab04f57aa326aedbe8
Fran Palmero <sweetydesigns@gmail.com>
2020-06-18 00:49:36 +0200

    gohan: thermal: Fix thermal-engine.conf path
    04-06 19:09:36.773   475   475 E ThermalEngine: Unable to open config file '/system/etc/thermal-engine.conf'

--- device.mk
+++ device.mk
@@ -311,7 +311,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/system/etc/thermal-engine.conf
 
 # Vendor prebuilts
 PRODUCT_COPY_FILES += \
